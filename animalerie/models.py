77313ee02d457fc60102135884ff2a3ef0575b2b
Junyi <junyi.wang@centrale-marseille.fr>
2021-11-26 12:32:30 +0100

    Animalerie, Junyi

---
+++ animalerie/models.py
@@ -0,0 +1,101 @@
+from django.db import models
+
+class Equipement(models.Model):
+    id_equip = models.CharField(max_length=100, primary_key=True)
+    disponibilite = models.CharField(max_length=20)
+    photo = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.id_equip
+
+    def occupants(self):
+        occupants = []
+        for animal in Animal.objects.all():
+            if animal.lieu.id_equip == self.id_equip:
+                occupants.append(animal)
+        return occupants
+
+
+class Animal(models.Model):
+    id_animal = models.CharField(max_length=100, primary_key=True)
+    etat = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    race = models.CharField(max_length=20)
+    photo = models.CharField(max_length=200)
+    lieu = models.ForeignKey(Equipement, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id_animal
+
+    def nourrir(self):
+        if self.etat == "affamé":
+            mangeoire = Equipement.objects.get(id_equip='mangeoire')
+            if mangeoire.disponibilite == "libre":
+                self.lieu.disponibilite = "libre"
+                self.lieu.save()
+
+                self.lieu = mangeoire
+                self.etat = "repus"
+                self.save()
+
+                mangeoire.disponibilite = "occupé"
+                mangeoire.save()
+
+                return "info", "Félicitation, " + self.id_animal + " a rejoint la mangeoire et est maintenant repus."
+            else:
+                return "danger", "Impossible, la mangeoire est actuellement occupée par " + mangeoire.occupants()[0].id_animal + ". "
+        else:
+            return "danger", "Désolé, " + self.id_animal + " n'a pas faim!"
+
+    def divertir(self):
+        if self.etat == "repus":
+            roue = Equipement.objects.get(id_equip='roue')
+            if roue.disponibilite == "libre":
+                self.lieu.disponibilite = "libre"
+                self.lieu.save()
+
+                self.lieu = roue
+                self.etat = "fatigué"
+                self.save()
+
+                roue.disponibilite = "occupé"
+                roue.save()
+
+                return "info", "Félicitation, " + self.id_animal + " a rejoint la roue et est maintenant fatigué."
+            else:
+                return "danger", "Impossible, la roue est actuellement occupée par " + roue.occupants()[0].id_animal + "."
+        else:
+            return "danger", "Désolé, " + self.id_animal + " n'est pas en état de faire du sport!"
+
+    def coucher(self):
+        if self.etat == "fatigué":
+            nid = Equipement.objects.get(id_equip='nid')
+            if nid.disponibilite == "libre":
+                self.lieu.disponibilite = "libre"
+                self.lieu.save()
+
+                self.lieu = nid
+                self.etat = "endormi"
+                self.save()
+
+                nid.disponibilite = "occupé"
+                nid.save()
+
+                return "info", "Félicitation, " + self.id_animal + " a rejoint le nid et est maintenant endormi."
+            else:
+                return "danger", "Impossible, la nid est actuellement occupée par " + nid.occupants()[0].id_animal + "."
+        else:
+            return "danger", "Désolé, " + self.id_animal + " n'est pas fatigué!"
+
+    def reveiller(self):
+        if self.etat == "endormi":
+            self.lieu.disponibilite = "libre"
+            self.lieu.save()
+
+            self.lieu = Equipement.objects.get(id_equip='litiere')
+            self.etat = "affamé"
+            self.save()
+
+            return "info", "Félicitation, " + self.id_animal + " a rejoint la litière et est maintenant affamé."
+        else:
+            return "danger", "Désolé, " + self.id_animal + " ne dort pas!"
---
+++ animalerie/views.py
@@ -0,0 +1,55 @@
+from .models import Animal, Equipement
+from .forms import MoveForm
+from django.shortcuts import render, get_object_or_404, redirect
+
+def animal_list(request):
+    animaux = Animal.objects.all()
+    equipements = Equipement.objects.all()
+    message_type, message = "", ""
+
+    if request.method == "GET":
+        if 'animal' and 'equipement' in request.GET:
+            id_equip = request.GET['equipement']
+            animal = Animal.objects.get(id_animal=request.GET['animal'])
+            if id_equip == "litiere":
+                message_type, message = animal.reveiller()
+            elif id_equip == "mangeoire":
+                message_type, message = animal.nourrir()
+            elif id_equip == "roue":
+                message_type, message = animal.divertir()
+            elif id_equip == "nid":
+                message_type, message = animal.coucher()
+
+    return render(request, 'animalerie/animal_list.html', {
+        'animaux': animaux,
+        'equipements': equipements,
+        'type': message_type,
+        'message': message
+    })
+
+
+def animal_detail(request, id_animal):
+    animal = get_object_or_404(Animal, id_animal=id_animal)
+    message_type, message = "", ""
+
+    if request.method == "GET":
+        if 'action' in request.GET:
+            action = request.GET["action"]
+            if action == "reveiller":
+                message_type, message = animal.reveiller()
+            elif action == "nourrir":
+                message_type, message = animal.nourrir()
+            elif action == "divertir":
+                message_type, message = animal.divertir()
+            elif action == "coucher":
+                message_type, message = animal.coucher()
+
+    return render(request, 'animalerie/animal_detail.html', {
+        'animal': animal,
+        'type': message_type,
+        'message': message
+    })
+
+def equipement_detail(request, id_equip):
+    equipement = get_object_or_404(Equipement, id_equip=id_equip)
+    return render(request, 'animalerie/equipement_detail.html', {'equipement': equipement})
